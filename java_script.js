d445e806b01672f91769d0db20c6339389a2a530
Akuma <omarwalid31@outlook.fr>
2026-02-04 09:45:39 +0100

    commit

--- java_script.js
+++ java_script.js
@@ -2,21 +2,13 @@ let clicks = 0;
 let bloodShown = false;
 let warningChanged = false;
 
-
-  const r = Math.floor(Math.random() * 120);
-  const g = Math.floor(Math.random() * 120);
-  const b = Math.floor(Math.random() * 120);
-  return `rgb(${r}, ${g}, ${b})`;
-
-eye.addEventListener("click", () =>  
-  clicks++
-
-
-  
+// éléments HTML
+const eye = document.getElementById("eye");
 const counter = document.getElementById("counter");
 const blood = document.getElementById("blood");
 const warning = document.getElementById("warning");
 
+// fonction couleur sombre aléatoire
 function randomDarkColor() {
   const r = Math.floor(Math.random() * 120);
   const g = Math.floor(Math.random() * 120);
@@ -24,6 +16,7 @@ function randomDarkColor() {
   return `rgb(${r}, ${g}, ${b})`;
 }
 
+// clic sur l'œil
 eye.addEventListener("click", () => {
   clicks++;
   counter.textContent = "Clics : " + clicks;
@@ -31,11 +24,13 @@ eye.addEventListener("click", () => {
   // changement de fond
   document.body.style.backgroundColor = randomDarkColor();
 
+  // sang à 100
   if (clicks >= 100 && !bloodShown) {
     blood.style.opacity = "1";
     bloodShown = true;
   }
 
+  // changement de phrase à 100
   if (clicks >= 100 && !warningChanged) {
     warning.textContent = "arrete toi immédiatement !";
     warning.style.color = "#ff0000";
@@ -43,5 +38,3 @@ eye.addEventListener("click", () => {
     warningChanged = true;
   }
 });
-
-
